a7c16d5569fae43e52966182b43f5e649d54e8b4
garima35 <garthapa@gmail.com>
2016-08-11 15:47:14 -0700

    My First Commit
    first file

---
+++ portfolio-html.js
@@ -0,0 +1,9 @@
+        function changeImg(){
+            var img = document.getElementById("Hi");
+            img.src = "paddleball.png";
+            
+        }
+function Loading(){
+    
+alert("Welcome to my page!");
+}
